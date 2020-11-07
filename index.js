57770b1d2d50f20d43fe08ba39bb70e326defb3f
aleebrady <aleebrady@gmail.com>
2020-11-07 11:33:44 -0800

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => (tutorial.split(" ").map(cap => cap.charAt(0).toUpperCase() + cap.slice(1)).join(' ')))
 }
